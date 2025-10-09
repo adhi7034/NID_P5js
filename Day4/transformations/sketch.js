44cb1c2d784a25cf9fdf21cf97ae561cc5dc30dd
adhi7034 <adithyankr100@gmail.com>
2025-10-09 15:10:56 +0530

    update

--- Day4/transformations/sketch.js
+++ Day4/transformations/sketch.js
@@ -1,8 +1,8 @@
-let noPetal = 0;
-let x = 0;
-let y = 0;
+// let noPetal = 0;
+// let x = 0;
+// let y = 0;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(innerWidth, innerHeight);
   angleMode(DEGREES);
   
 }
@@ -10,18 +10,20 @@ function setup() {
 function draw() {
   background(220);
   drawFlower(14, 200, 200);
+  drawFlower(14, 100, 500);
 }
 function drawFlower(noPetal, x, y) {
   push();
   translate(x, y);
   rotate(frameCount);
 
-  for (let i = 0; i < noPetal; i=i+1); {
+  for (let i = 0; i < noPetal; i=i+1) {
     
-
+    //console.log(i);
     fill(255, 0, 0, 100);
-    ellipse(100, 0, 200, 25);
+    ellipse(20, 0, 200, 25);
     rotate(360 / noPetal);
+    
   }
   fill(0, 255, 255);
   ellipse(0, 0, 50);
