9ae4260098c8bb374aad2af53fe430676883ba6b
adhi7034 <adithyankr100@gmail.com>
2025-10-11 20:24:02 +0530

    update

--- Day4/image_loop/sketch.js
+++ Day4/image_loop/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 }
 function preload() {
   for (let i=0; i<noImages;i++ ){
-    let name = "img"+i+".png";
+    let name = "images2/img"+i+".png";
     genImages[i]= loadImage(name);
   }
   // img0 = loadImage('images2/patterns0.png');
